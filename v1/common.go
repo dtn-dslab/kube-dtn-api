9d7ab92fa5a57acebdda18405317ac099e2ca403
Shuyang Xie <xsy1323541772@163.com>
2024-11-22 16:06:52 +0800

    fix: modify api

--- v1/common.go
+++ v1/common.go
@@ -1,6 +1,12 @@
 package v1
 
 const (
+	K8sNodeRoleKubeDTNKey       = "role"
+	K8sNodeRoleKubeDTNHostValue = "kubedtn-host"
+	K8sNodeRoleKubeDTNDPUValue  = "kubedtn-dpu"
+)
+
+const (
 	InternalLinkDirectionSender   = "sender"
 	InternalLinkDirectionReceiver = "receiver"
 )
